e4e48db818cd4edf102011c1f353093d77f1bc2e
DaTTV <admin@freezy.me>
2025-02-16 18:49:38 +0100

    Added clan manager! Loading all storage files

--- src/main/java/me/freezy/plugins/papermc/blazesmp/BlazeSMP.java
+++ src/main/java/me/freezy/plugins/papermc/blazesmp/BlazeSMP.java
@@ -1,11 +1,25 @@
 package me.freezy.plugins.papermc.blazesmp;
 
+import lombok.Getter;
+import me.freezy.plugins.papermc.blazesmp.module.manager.Clans;
+import me.freezy.plugins.papermc.blazesmp.module.manager.Homes;
+import me.freezy.plugins.papermc.blazesmp.module.manager.ProtectedBlocks;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BlazeSMP extends JavaPlugin {
+    @Getter private static BlazeSMP instance;
+    @Getter private Homes homes;
+    @Getter private ProtectedBlocks protectedBlocks;
+    @Getter private Clans clans;
 
     @Override
     public void onLoad() {
+        this.homes=new Homes();
+        this.homes.load();
+        this.protectedBlocks=new ProtectedBlocks();
+        this.protectedBlocks.load();
+        this.clans=new Clans();
+        this.clans.loadAllClans();
     }
 
     @Override
