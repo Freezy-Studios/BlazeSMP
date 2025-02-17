8fe081424f2a2c38ee620c67f6a119d450fccfb8
DaTTV <104141141+DrFreezyYT@users.noreply.github.com>
2025-02-17 19:28:46 +0100

    Blocked tabcomplete for some commands! Fixed message bug!

--- src/main/java/me/freezy/plugins/papermc/blazesmp/listener/PlayerJoinListener.java
+++ src/main/java/me/freezy/plugins/papermc/blazesmp/listener/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package me.freezy.plugins.papermc.blazesmp.listener;
 
 import me.freezy.plugins.papermc.blazesmp.manager.PlayerManager;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,5 +12,7 @@ public class PlayerJoinListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         new PlayerManager().setPlayerTeam(player);
+
+        event.joinMessage(MiniMessage.miniMessage().deserialize("<gray>[<green>+</green>]</gray> <reset>").append(player.teamDisplayName()));
     }
 }
